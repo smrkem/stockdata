cfc523b58cefe30d1dd970663836318612be7548
Matt Smrke <matthew.smrke@empire.ca>
2016-05-20 17:42:17 -0400

    delete dumb names

--- web/djandocker/settings_local.py
+++
@@ -1,9 +0,0 @@
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'aura_portal',
-        'USER': 'postgres',
-        'PORT': 5432,
-    }
-}
