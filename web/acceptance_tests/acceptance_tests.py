da12514e63adbaac592998ece5c38fe5f9f396a8
Matt Smrke <matthew.smrke@empire.ca>
2016-09-28 22:00:50 -0400

    target first FT

---
+++ web/acceptance_tests/acceptance_tests.py
@@ -0,0 +1,24 @@
+import unittest
+from selenium import webdriver as wd
+
+LIVE_SERVER_URL = 'http://localhost:8000/'
+
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_app_is_running(self):
+        self.browser = wd.Firefox()
+        self.browser.implicitly_wait(3)
+        self.browser.get(LIVE_SERVER_URL)
+        self.assertEqual(1, 0)
+        self.browser.quit()
+
+
+if __name__ == '__main__':
+    unittest.main()
