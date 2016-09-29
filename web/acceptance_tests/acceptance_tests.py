00008a9e2daa64a02453285986c270259e3ef0bc
Matt Smrke <matthew.smrke@empire.ca>
2016-09-28 22:05:57 -0400

    woot - first passing FT inside docker

--- web/acceptance_tests/acceptance_tests.py
+++ web/acceptance_tests/acceptance_tests.py
@@ -1,7 +1,4 @@
 import unittest
-from selenium import webdriver as wd
-
-LIVE_SERVER_URL = 'http://localhost:8000/'
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -12,12 +9,8 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_app_is_running(self):
-        self.browser = wd.Firefox()
-        self.browser.implicitly_wait(3)
-        self.browser.get(LIVE_SERVER_URL)
+    def test_test_is_running(self):
         self.assertEqual(1, 0)
-        self.browser.quit()
 
 
 if __name__ == '__main__':
