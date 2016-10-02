aa39285f7c116dd6ce552cbcfd3691b419e0bd2f
Matt Smrke <matthew.smrke@empire.ca>
2016-10-02 14:05:47 -0400

    initial working xvfb

--- web/acceptance_tests/acceptance_tests.py
+++ web/acceptance_tests/acceptance_tests.py
@@ -1,4 +1,7 @@
 import unittest
+from selenium import webdriver as wd
+
+LIVE_SERVER_URL = 'http://localhost:8000/'
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -10,7 +13,13 @@ class NewVisitorTest(unittest.TestCase):
         pass
 
     def test_test_is_running(self):
+        self.browser = wd.Firefox()
+        self.browser.implicitly_wait(3)
+        self.browser.get(LIVE_SERVER_URL)
+        print(self.browser.page_source)
+        print("please?...")
         self.assertEqual(1, 0)
+        self.browser.quit()
 
 
 if __name__ == '__main__':
