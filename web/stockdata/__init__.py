00a9359e66e1ad2ae73e6d1a7df1670176eda66c
Matt Smrke <matthew.smrke@empire.ca>
2016-10-06 19:41:46 -0400

    updates ch1

--- web/stockdata/__init__.py
+++ web/stockdata/__init__.py
@@ -7,9 +7,5 @@ app.config.from_object(BaseConfig)
 
 from stockdata import views
 
-def create_app(config_module=None):
-    appi = Flask(__name__)
-    return appi
-
 if __name__ == '__main__':
     app.run()
