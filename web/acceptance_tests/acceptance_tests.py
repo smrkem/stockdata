f2eaff564b032f6ad3b464734f49e2749f7507d7
Matt Smrke <matthew.smrke@empire.ca>
2016-10-06 22:04:44 -0400

    passing FT and first unit test

--- web/acceptance_tests/acceptance_tests.py
+++ web/acceptance_tests/acceptance_tests.py
@@ -1,11 +1,10 @@
 import unittest
 from selenium import webdriver as wd
-from flask import template_rendered
 from flask_testing import LiveServerTestCase, TestCase
 
 # Path hack.
-import sys, os
-sys.path.insert(0, os.path.abspath('.'))
+# import sys, os
+# sys.path.insert(0, os.path.abspath('.'))
 
 from stockdata import app
 
@@ -13,39 +12,36 @@ from stockdata import app
 LIVE_SERVER_URL = 'http://localhost:5000/'
 
 
-class NewVisitorTest(TestCase):
+class ViewsUnitTest(TestCase):
 
     def create_app(self):
         app.config['TESTING'] = True
         return app
 
+    def test_home_view_calls_index_template(self):
+        response = self.client.get('/')
+        self.assert_template_used('index.html')
+
+
+class NewVisitorTest(LiveServerTestCase):
+
+    def create_app(self):
+        app.config['TESTING'] = True
+        app.config['LIVESERVER_PORT'] = 8943
+        return app
+
     def setUp(self):
-        print('setUP')
         self.browser = wd.Firefox()
         self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
-        pass
 
-    # def _add_template(self, app, template, context):
-    #     print("MADE IT HERE")
-    #     if len(self.templates) > 0:
-    #         self.templates = []
-    #     self.templates.append((template, context))
+    def test_can_visit_homepage(self):
+        self.browser.get(self.get_server_url())
 
-    def test_test_is_running(self):
-        print("HERE?")
-        # print(self.get_server_url())
-        response = self.browser.get(LIVE_SERVER_URL)
-        # response = self.browser.get(self.get_server_url())
-        # self.assertRegex(self.browser.page_source, r'^<html(.*)</html>$')
-        # template_rendered.connect(self._add_template)
-        # print(self.templates)
-        print(response)
-        print(type(response))
-        self.assert_template_used('index.html')
-        self.fail('ok')
+        self.assertEqual("StockData", self.browser.title)
+        self.assertEqual("Hello, Karl", self.browser.find_element_by_tag_name('h1').text)
 
 
 if __name__ == '__main__':
