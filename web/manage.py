e78bc2b9743202cfdcea689520d90e1b3d485b53
Matt Smrke <matthew.smrke@empire.ca>
2016-11-12 14:39:39 -0500

    updates to use manage.py

--- web/manage.py
+++ web/manage.py
@@ -1,8 +1,18 @@
-from flask_script import Manager
+from flask_script import Manager, Server
 from stockdata import app
 import unittest
 
 manager = Manager(app)
+manager.add_command("runserver", Server(host='0.0.0.0'))
+
+@manager.command
+def test():
+    """Runs all tests in the tests/ folder."""
+    tests = unittest.TestLoader().discover('tests', pattern='*tests.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
 
 if __name__ == '__main__':
-    manager.run(debug=True, host='0.0.0.0')
+    manager.run()
