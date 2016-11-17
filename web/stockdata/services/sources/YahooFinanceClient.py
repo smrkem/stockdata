18578bf2142ab80483d09de153521668ebf0cc14
matt <smrkem@gmail.com>
2016-11-16 20:58:49 -0500

    adds price and yr high

--- web/stockdata/services/sources/YahooFinanceClient.py
+++ web/stockdata/services/sources/YahooFinanceClient.py
@@ -8,10 +8,14 @@ class YahooFinanceClient:
         stock_name = stock.get_name()
         if stock_name is None:
             return None
-
         stock_exchange = stock.get_stock_exchange()
+        current_price = stock.get_price()
+        year_high = stock.get_year_high()
+
         return {
             "symbol": symbol,
             "name": stock_name,
-            "exchange": stock_exchange
+            "exchange": stock_exchange,
+            "current_price": current_price,
+            "year_high": year_high
         }
